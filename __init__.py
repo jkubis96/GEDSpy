7e9c5ce9ac24d08ba343af83ddf9025d04936761
jkubis96 <jakub.kubis1996@gmail.com>
2022-01-09 14:59:02 +0100

    scripts

---
+++ __init__.py
@@ -0,0 +1,5 @@
+import GEDSpy.GOPa.GOPs.py
+import GEDSpy.zinc.zinc.py
+import numpy as np
+import pandas as pd
+import os
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'GEDSpy'
+LONG_DESCRIPTION = 'GEDSpy is the python library for gene list enrichment with genes ontology, pathways and potential drugs'
+
+# Setting up
+setup(
+        name="GEDSpy", 
+        version=VERSION,
+        author="Jakub Kubis",
+        author_email="jbiosystem@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['requests', 'ast', 'json', 'bioservices', 'pandas', 'datatime', 
+        'collections', 'tqdm', 'os', 'seaborn', 'matplotlib', 'scipy', 'nrtworkx', 'pyvis'], 
+        
+        keywords=['python', 'GO', 'pathways', 'drug', 'gene ontology'],
+        license 'MIT''
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: Linux :: Ubuntu",
+        ],
+        python_requires='>=3.6',
+)
+
+
