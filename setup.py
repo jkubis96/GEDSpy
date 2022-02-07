1da31183ea38e9fd051380f6440c9c3ee79fead8
jkubis96 <jakub.kubis1996@gmail.com>
2022-02-07 01:22:26 +0100

    v.1.0.4

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.3' 
+VERSION = '1.0.4' 
 DESCRIPTION = 'GEDSpy'
 LONG_DESCRIPTION = 'GEDSpy is the python library for gene list enrichment with genes ontology, pathways and potential drugs. Package description  on https://github.com/jkubis96/GEDSpy'
 
